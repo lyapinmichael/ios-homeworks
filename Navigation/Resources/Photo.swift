908002379e80b99b32e149df240a7656cf5a5718
Michael Lyapin <lyapin.dev@gmail.com>
2023-03-08 11:11:10 +0300

    Photos folder populated with content

--- Navigation/Resources/Photo.swift
+++ Navigation/Resources/Photo.swift
@@ -17,7 +17,24 @@ extension Photo {
             Photo(name: "Vader1"),
             Photo(name: "Vader2"),
             Photo(name: "Vader3"),
-            Photo(name: "Vader4")
+            Photo(name: "Vader4"),
+            Photo(name: "TieBomber"),
+            Photo(name: "AT-AT"),
+            Photo(name: "Vader5"),
+            Photo(name: "CoruscantSenateDistrict"),
+            Photo(name: "Geonosis"),
+            Photo(name: "Tidirium"),
+            Photo(name: "StarDestroyer"),
+            Photo(name: "Operation"),
+            Photo(name: "OnCorvette"),
+            Photo(name: "TrenchPursuit"),
+            Photo(name: "Vader6"),
+            Photo(name: "Vader7"),
+            Photo(name: "Vader8"),
+            Photo(name: "AnakinWithMother"),
+            Photo(name: "AnakinWithPadme"),
+            Photo(name: "Vader9")
+            
         ]
     }
 }

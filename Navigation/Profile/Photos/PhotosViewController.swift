ef8ea5c8c2fde8ac830afa8b89cefb7ee07cf5ca
Michael Lyapin <lyapin.dev@gmail.com>
2023-03-08 11:53:50 +0300

    PhotosCollectionView view controller background color changed to .white

--- Navigation/Profile/Photos/PhotosViewController.swift
+++ Navigation/Profile/Photos/PhotosViewController.swift
@@ -49,6 +49,7 @@ final class PhotosViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .white
         setupPhotosCollection()
         addSubviews()
         setConstraints()
